479e092531a2e31b8b6e10f5ea3313adf7ae30a1
SVadimko <144110@gmail.com>
2020-08-23 23:51:47 +0300

    began_menu_building

--- app/src/main/java/com/example/guess_for_max/MainActivity.java
+++ app/src/main/java/com/example/guess_for_max/MainActivity.java
@@ -12,32 +12,36 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
-
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private EditText gues_fld;
     private Button gues_btn;
     private TextView info;
-    private int gues;
     private int previos=0;
     private int count=1;
     private int theNumber;// = (int) (Math.random() * 100.0D + 1.0D);
     boolean won;
     String info_msg="";
     int color= Color.BLACK;
-    ArrayList<String> motivation;
+    //ArrayList<String> motivation;
     String []motiv_quotes={"Давай же!","У тебя получится!","Ты в правильном направлении!",
             "Уже почти!", "Ответ близок!", "Постарайся!", "Еще чуть-чуть!", "Почти угадал!"};
 
 
     public void new_game(){
         theNumber=(int) (Math.random() * 100.0D + 1.0D);
-        motivation=new ArrayList<String>(Arrays.asList(motiv_quotes));
+        //motivation=new ArrayList<String>(Arrays.asList(motiv_quotes));
+        previos=0;
+        count=1;
+        color=Color.BLACK;
+        info_msg="Введите число и нажмите кнопку!";
+        info.setText(info_msg);
+        info.setTextColor(color);
+        gues_fld.requestFocus();
+        gues_fld.setText("");
     }
     public void exit_app() {
         System.exit(0);
@@ -45,63 +49,52 @@ public class MainActivity extends AppCompatActivity {
 
     public void check_guess(){
         color=Color.BLACK;
-
         try {
-            gues = Integer.parseInt(gues_fld.getText().toString());
-            if ((gues>=0&gues<=100)&previos != gues) {
+            int gues = Integer.parseInt(gues_fld.getText().toString());
+            if ((gues >=0& gues <=100)&previos != gues) {
                 if (theNumber == gues) {
-                    //info.setText("Вы угадали! Попыток: " + count);
                     info_msg="Вы угадали! Попыток: " + count;
                     info.setText(info_msg);
                     //info.setIcon(new ImageIcon("congrats.png"));
+                    Toast.makeText(MainActivity.this, "МОЛОДЕЦ!!!", Toast.LENGTH_LONG).show();
                     gues_btn.setText("Выход");
                     won = true;
                 } else if (gues < theNumber) {
-                    //info.setText("Загаданное число больше! Попыток: " + count);
                     info_msg="Загаданное число больше! Попыток: " + count;
                     info.setText(info_msg);
-                    int motiv_quot_indx=(int)(Math.random() * 7);
-                    Toast.makeText(MainActivity.this, motivation.get(motiv_quot_indx), Toast.LENGTH_LONG).show();
+                    int motiv_quot_indx=(int)(Math.random() * motiv_quotes.length);
+                    Toast.makeText(MainActivity.this, motiv_quotes[motiv_quot_indx], Toast.LENGTH_LONG).show();
                     //info.setIcon(new ImageIcon("up.png"));
                 } else {
-                    //info.setText("Загаданное число меньше! Попыток: " + count);
                     info_msg="Загаданное число меньше! Попыток: " + count;
                     info.setText(info_msg);
-                    int motiv_quot_indx=(int)(Math.random() * 7);
-                    Toast.makeText(MainActivity.this, motivation.get(motiv_quot_indx), Toast.LENGTH_LONG).show();
+                    int motiv_quot_indx=(int)(Math.random() * motiv_quotes.length);
+                    Toast.makeText(MainActivity.this, motiv_quotes[motiv_quot_indx], Toast.LENGTH_LONG).show();
                     //info.setIcon(new ImageIcon("down.png"));
                 }
                 ++count;
-                Toast.makeText(MainActivity.this, "МОЛОДЕЦ!!!", Toast.LENGTH_LONG).show();
-            } else if (0>gues|gues>100) {
-                //info.setText("Ответ должен быть числом из диапазона 0-100");
+
+            } else if (0> gues | gues >100) {
                 info_msg="Ответ должен быть числом из диапазона 0-100";
                 color=Color.RED;
                 info.setText(info_msg);
                 // info.setIcon(new ImageIcon("range.png"));
-                //info.setForeground(Color.RED);
-                //info.setTextColor(#3F51B5);
             }
             else {
-                //info.setText("Повторный ответ!");
                 info_msg="Повторный ответ!";
                 color=Color.RED;
                 info.setText(info_msg);
                 // info.setIcon(new ImageIcon("wrong.png"));
-                // info.setForeground(Color.RED);
             }
             previos = gues;
         } catch (NumberFormatException ex) {
-            //info.setText("Ответ должен быть числом из диапазона 0-100");
             info_msg="Ответ должен быть числом из диапазона 0-100";
             color=Color.RED;
             info.setText(info_msg);
             // info.setIcon(new ImageIcon("no_let.png"));
-            // info.setForeground(Color.RED);
         }
         gues_fld.requestFocus();
         gues_fld.selectAll();
-        //info.setText(info_msg);
         info.setTextColor(color);
     }
 
@@ -109,9 +102,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gues_fld = (EditText) findViewById(R.id.gues_fld);
-        gues_btn = (Button) findViewById(R.id.gues_btn);
-        info = (TextView) findViewById(R.id.info);
+        gues_fld = findViewById(R.id.gues_fld);
+        gues_btn = findViewById(R.id.gues_btn);
+        info = findViewById(R.id.info);
         new_game();
         gues_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -161,13 +154,27 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        //if (id == R.id.action_settings) {
+        //    return true;
+       // }
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_newgame:{
+                new_game();
+                return true;}
+            case R.id.action_gamestats:
+                //return true;
+            case R.id.action_about:
+                return true;
+            case R.id.action_exit:{
+                exit_app();
+                return true;}
+            default:
         return super.onOptionsItemSelected(item);
+        }
     }
 }
